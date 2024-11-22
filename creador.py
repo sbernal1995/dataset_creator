81d29f1cd4335efc584cbbdbe17e2b9fa7738b8d
Sebastian Bernal <39896829+sbernal1995@users.noreply.github.com>
2024-11-21 21:02:48 -0300

    Test leer video

---
+++ creador.py
@@ -0,0 +1,29 @@
+import cv2
+
+def main():
+
+    video_path = 'sea.mp4'
+
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print("Error al abrir el video")
+        return
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Fin del video")
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
